9253d113c6ab5e31cf5b1629d1f8d618b1a2f966
luoxin <A33@mlf.com>
2021-04-02 18:11:00 +0800

    ## 类

--- p01-start/p01-start.js
+++ p01-start/p01-start.js
@@ -1,15 +1,44 @@
 // npx tsc p01 - start.ts
 // ## 类型注解
-function greeter(person) {
-    return "Hello, " + person;
+function typeFn() {
+    function greeter(person) {
+        return "Hello, " + person;
+    }
+    var user = "Jane User";
+    // let user = [1, 2, 3]; // 参数类型错误
+    document.body.innerHTML = greeter(user);
+    // document.body.innerHTML = greeter(user, 3); // 参数数量错误
 }
-var user = "Jane User";
-// let user = [1, 2, 3]; // 参数类型错误
-document.body.innerHTML = greeter(user);
-function greeter1(person) {
-    return "Hello," + person.firstName + person.lastNage;
+typeFn();
+// ## 接口
+function interfaceFn() {
+    function greeter(person) {
+        return "Hello," + person.firstName + person.lastName;
+    }
+    var user = {
+        firstName: "Jane",
+        lastName: "User",
+        test: "aaa"
+    };
+    document.body.innerHTML = greeter(user);
 }
-var user1 = {
-    firstName: "Jane"
-};
-document.body.innerHTML = greeter1(user1);
+interfaceFn();
+// ## 类
+// 在构造函数的参数上使用public等同于创建了同名的成员变量。
+function classFn() {
+    var Student = /** @class */ (function () {
+        function Student(firstName, middleInitial, lastName) {
+            this.firstName = firstName;
+            this.middleInitial = middleInitial;
+            this.lastName = lastName;
+            this.fullName = [firstName, middleInitial, lastName].join(" ");
+        }
+        return Student;
+    }());
+    function greeter(person) {
+        return "Hello, " + person.firstName + " " + person.lastName;
+    }
+    var user = new Student("Jane", "M.", "User");
+    document.body.innerHTML = greeter(user);
+}
+classFn();
--- p01-start/p01-start.ts
+++ p01-start/p01-start.ts
@@ -1,28 +1,58 @@
 // npx tsc p01 - start.ts
 
 // ## 类型注解
-function greeter(person: string) {
-    return "Hello, " + person;
+function typeFn() {
+    function greeter(person: string) {
+        return "Hello, " + person;
+    }
+    
+    let user = "Jane User";
+    // let user = [1, 2, 3]; // 参数类型错误
+    
+    document.body.innerHTML = greeter(user);
+    // document.body.innerHTML = greeter(user, 3); // 参数数量错误
 }
-
-let user = "Jane User";
-// let user = [1, 2, 3]; // 参数类型错误
-
-document.body.innerHTML = greeter(user);
-// document.body.innerHTML = greeter(user, 3); // 参数数量错误
+typeFn()
 
 // ## 接口
-interface Person {
-    firstName: string,
-    lastNage: String;
+function interfaceFn() {
+    interface Person {
+        firstName: string,
+        lastName: String;
+    }
+    
+    function greeter(person: Person) {
+        return "Hello," + person.firstName + person.lastName
+    }
+    let user = {
+        firstName: "Jane",
+        lastName: "User",
+        test: "aaa", //缺少字段会报错 多出不影响
+    }
+    document.body.innerHTML = greeter(user)
 }
+interfaceFn()
 
-function greeter1(person: Person) {
-    return "Hello," + person.firstName + person.lastNage
+// ## 类
+// 在构造函数的参数上使用public等同于创建了同名的成员变量。
+function classFn() {
+    class Student {
+        fullName: string;
+        constructor(public firstName, public middleInitial, public lastName) {
+            this.fullName = [firstName, middleInitial, lastName].join(" ")
+        }
+    }
+
+    interface Person {
+        firstName: string;
+        lastName: string;
+    }
+
+    function greeter(person: Person) {
+        return "Hello, " + person.firstName + " " + person.lastName
+    }
+
+    let user = new Student("Jane", "M.", "User")
+    document.body.innerHTML = greeter(user) // 类参数使用interface检查
 }
-let user1 = {
-    firstName: "Jane",
-    lastNage: "User",
-    test: "aaa", //缺少字段会报错 多出不影响
-}
-document.body.innerHTML = greeter1(user1)
+classFn()
