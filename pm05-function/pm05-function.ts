55ba33cca32252c35421440926385ccfdc5e9ffa
thetime50 <1004763777@qq.com>
2021-12-31 04:04:32 +0800

    pm05 back

--- pm05-function/pm05-function.ts
+++ pm05-function/pm05-function.ts
@@ -2,7 +2,32 @@
 // npx tsc --pretty
 // --noImplicitThis
 
+function funTypeDefine(){
+    function add(x:number, y:number):number{
+        return x + y
+    }
+    let myAdd = function(x:number, y:number):number{return x + y}
 
+    // 完整书写
+    let myAdd2: (baseValue: number, increment: number) => number =
+        function(x: number, y: number): number { return x + y }
+}
+
+function typeInfer(){
+    let myAdd2 = function(x: number, y: number): number { return x + y }
+    let myAdd: (x: number, y: number) => number = function(x, y) { return x + y }
+}
+
+// 可选参数和默认参数
+function kexuanCanshuMorenCanshu(){
+    function buildName(firstName:string, lastName?:string){
+        return firstName + " " + lastName
+    }
+    function buildName1(firstName: string, lastName="Smith"){
+        return firstName + " " + lastName
+    }
+}
+// 可选参数和默认参数公用类型
 function kexuanMoren(){
     let fa = function (x: number, y: number) {
         return 'rrr'
@@ -20,4 +45,35 @@ function kexuanMoren(){
     let ft2: typeof fb = fc
 }
 
+// 剩余参数
+function shengyuCanshu(){
+    function buildName(firstName: string, ...restOfName: string[]) {
+        return firstName + " " + restOfName.join(" ");
+    }
+    let employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+}
+
+// 对this的声明
+function thisType() {
+    interface AddInterfack {
+        a: number;
+        b: number;
+        fn: (this: AddInterfack) => number;
+    }
+    let addObj: AddInterfack = {
+        a: 1,
+        b: 2,
+        fn: function () {
+            return this.a + this.b
+        }
+    }
+    console.log(addObj.fn())
+}
+
+funTypeDefine()
+typeInfer()
+kexuanCanshuMorenCanshu()
 kexuanMoren()
+shengyuCanshu()
+thisType()
+
