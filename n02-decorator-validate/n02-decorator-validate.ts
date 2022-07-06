ec2b8c8ac984fad066c645c54ca7dc5eb6ed452d
thetime50 <thetime_lx@qq.com>
2022-07-06 16:42:13 +0800

    完成类型运行时检查

--- n02-decorator-validate/n02-decorator-validate.ts
+++ n02-decorator-validate/n02-decorator-validate.ts
@@ -2,10 +2,11 @@
 
 // https://yqz0203.github.io/decorator-and-reflect-metadata-in-typescript/
 
-import { ArrayLiteralExpression } from "typescript"
+// import { ArrayLiteralExpression } from "typescript"
 import "reflect-metadata"
 
 function validateTest(){
+    console.log("\n/****validateTest****/")
     function Type(type: any){
         return Reflect.metadata('design:type', type)
     }
@@ -46,6 +47,89 @@ function validateTest(){
 }
 
 function validateTest2(){
+    console.log("\n/****validateTest2****/")
+    const validateClass: any = (target: any) => {
+        console.log('target == C', target == C)
+        // if( Reflect.getO )
+        const constructorValid:any = {} // 静态方法
+        for (let key of Object.getOwnPropertyNames(target)){
+            let validate = Reflect.getMetadata(VALIDMDKEY, target, key)
+            if (validate){
+                constructorValid[key] = validate
+            }
+        }
+        const prototypeValid:any = {} // 原型方法
+        for (let key of Object.getOwnPropertyNames(target.prototype)  ){
+            let validate = Reflect.getMetadata(VALIDMDKEY, target.prototype, key)
+            if (validate){
+                // const descriptor = Object.getOwnPropertyDescriptor(target.prototype, key)
+                prototypeValid[key] = validate
+            }
+        }
+        if (Object.keys(constructorValid).length || Object.keys(prototypeValid).length){
+            class ResClass extends target{
+                constructor(...args: any[]){
+                    super(...args)
+                    const that = this
+                    console.log('hello')
+                    for (let key in prototypeValid){
+                        let origin = Object.getOwnPropertyDescriptor(target.prototype , key)!
+                        if (origin.value) { // 方法属性
+                            if (that[key].prototype) { // function 函数
+                                Object.defineProperty(that, key, {
+                                    ...origin,
+                                    value:function (...args: any[]) {
+                                        (prototypeValid)[key](...args)
+                                        origin.value.apply(that, args)
+                                    }
+                                })
+                            } else { // 箭头函数
+                                Object.defineProperty(that, key, {
+                                    ...origin,
+                                    value:(...args: any[]) => {
+                                        (prototypeValid)[key](...args)
+                                        origin.value.apply(that, args)
+                                    }
+                                })
+                            }
+                        } else if (origin.set) { // accessor
+                            Object.defineProperty(that, key, {
+                                ...origin,
+                                set: function (...args: any[]) {
+                                    (prototypeValid)[key](...args)
+                                    origin.set && origin.set.apply(that, args)
+                                }
+                            })
+                        }
+                    }
+                }
+            }
+            if (Object.keys(constructorValid).length){
+                for (let key in constructorValid) {
+                    let origin = Object.getOwnPropertyDescriptor(target, key)!
+                    if (ResClass[key].prototype) { // function 函数
+                        Object.defineProperty(ResClass, key, {
+                            ...origin,
+                            value: function (...args: any[]){
+                                (constructorValid)[key](...args)
+                                origin.value.apply(ResClass, args)
+                            }
+                        })
+                    } else { // 箭头函数
+                        Object.defineProperty(ResClass, key, {
+                            ...origin,
+                            value: (...args: any[]) => {
+                                (constructorValid)[key](...args)
+                                origin.value.apply(ResClass, args)
+                            }
+                        })
+                    }
+                }
+            }
+            return ResClass
+        }
+    }
+    const VALIDMDKEY = '__validata:parameter'
     const validateDecorator = ( target:any, propertyKey: string, descriptor: PropertyDescriptor) => {
         /**
          * 1. 属性校验装饰器上绑定校验方法
@@ -55,23 +139,48 @@ function validateTest2(){
          */
 
         let types = Reflect.getMetadata('design:paramtypes', target, propertyKey) as any[] | undefined // ArrayLiteralExpression
-        console.log('propertyKey', propertyKey)
-        console.log('type', types)
-        let res = types && types[1] && Reflect.getMetadataKeys(types[1])
-        console.log('res', res)
-        const baseType = ['number', 'string', 'boolean', /* null, undefined , Symbol, BigInt */]
-        // if (types && types.length){
-        //     function parameterValidata(instance: any){
-        //         types && types.forEach((type , index:number) => {
-        //             if()
-        //             if (!(instance[index] instanceof type)){
-        //                 throw new TypeError(`Invalid type ${type}`)
-        //             }
-        //         })
-        //     }
-        //     Reflect.defineMetadata('__parameterValidata', parameterValidata, target, propertyKey)
-        // }
+        // 类型 bigint 联合类型 可变参数 无法通过design:paramtypes传递
+        const baseType = {//['number', 'string', 'boolean', /* null, undefined , Symbol, BigInt */]
+            number: Number,
+            string: String,
+            boolean: Boolean,
+            null: undefined,// null,
+            undefined: undefined,
+            symbol: Symbol,
+            bigint: BigInt,
+            // function: Function
+        }
+        if (types && types.length){
+            function parameterValidata(...args: any[]){
+                types && types.forEach((type , index:number) => {
+                    if (type !== Object ){
+                        if (baseType.hasOwnProperty(typeof args[index] )){
+                            if (type !==  (<any>baseType)[typeof args[index]]){
+                                throw new TypeError(`Invalid type ${typeof args[index]},need ${type}`)
+                            }
+                        } else if (typeof type == "function"){
+                            if (/^class /.test(type.toString())){ // 是类构造函数
+                                if (!(args[index] instanceof type)){
+                                    throw new TypeError(`Invalid type ${typeof args[index]},need ${type}`)
+                                }
+                            }else{ // 一般函数
+                                if(typeof args[index] !== 'function' ){
+                                    throw new TypeError(`Invalid type ${typeof args[index]},need ${type}`)
+                                }
+                            }
+                        }
+                    } else { // bigint 联合类型 可变参数
+                        // 
+                    }
+                })
+            }
+            console.log('target == C', target == C, target == C.prototype)
+            Reflect.defineMetadata(VALIDMDKEY, parameterValidata, target, propertyKey)
+        }
     }
+
+    class C0{}
+    @validateClass
     class C{
         private _name: String = '';
         constructor(){
@@ -91,11 +200,69 @@ function validateTest2(){
         add( a:number, b:number|string){
             return  a.toString() + b
         }
+
+        @validateDecorator
+        test(
+            num:number, str:string, bo: boolean, 
+            nu:null, un:undefined, sym:symbol,
+            /* bi: BigInt, */cb: (a: number, b: number) => number, c:C0, ...attr: [number,number]){
+
+            return cb.call(this, ...attr)
+        }
+        @validateDecorator
+        static staticTest(
+            num: number, str: string, bo: boolean,
+            nu: null, un: undefined, sym: symbol,
+            /* bi: BigInt, */ cb: (a: number, b: number) => number, c: C0, ...attr: [number, number]) {
+
+            return cb.call(this, ...attr)
+        }
     }
 
     const c = new C()
-    c.name = "10"
-    c.name = new String('10')
+    // c.name = "10"
+    // c.name = new String('10')
+
+    function getParams(){
+        return {
+            num: <any> 10,
+            str: <any> 'hello',
+            bo: <any> true,
+            nu: <any> null,
+            un: <any> undefined,
+            sym: <any> Symbol('symbol'),
+            /* bi: <any> BigInt(3), // 3n,// new BigInt(3) */
+            cb: <any> ((a: number, b: number) => a + b),
+            c: <any> new C0(),
+            attr: <[number, number]> [3,2]
+        }
+    }
+    function callTest(params: any){
+        console.log('/*callTest*/')
+        let res = c.test(
+            params.num, params.str, params.bo,
+            params.nu, params.un, params.sym,
+            /* params.bi, */ params.cb, params.c, ...params.attr as [number, number]
+        )
+        console.log('test',res)
+    }
+    function callStaticTest(params: any){
+        console.log('/*callStaticTest*/')
+        let res = C.staticTest(
+            params.num, params.str, params.bo,
+            params.nu, params.un, params.sym,
+            /* params.bi, */ params.cb, params.c, ...params.attr as [number, number]
+        )
+        console.log('staticTest',res)
+    }
+    let params = getParams()
+    callTest(params)
+    callStaticTest(params)
+
+    params = getParams()
+    params.num = '10'
+    // callTest(params) // 报类型错误
+    // callStaticTest(params) // 报类型错误
 }
 
 validateTest()
